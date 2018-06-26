b1ebf7ed75f9bec3cd889695f5e4eee6f6cd4d54
U-SCIENCE\smit18 <smit18@ELSSTLW-7003333.science.regn.net>
2018-06-26 09:56:45 -0500

    Changed jQuery selectors

--- BackofficeTools.user.js
+++ BackofficeTools.user.js
@@ -9,16 +9,16 @@
 // @require      http://code.jquery.com/jquery-1.12.4.min.js
 // ==/UserScript==
 
-$(document).ready( function () {
+$(document).ready(function() {
     //Clone, enable, and align evolve_user_id field
     var userId = $('#evolve_user_id').clone().attr('type', 'text').attr('disabled', 'true').attr('id', 'evolve_user_id_COPY').wrap('<td valign="top"></td>').parent();
-    $('#accountForm > table > tbody > tr:nth-child(6) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(9) > td').after(userId);
-    $('#accountForm > table > tbody > tr:nth-child(6) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(9) > td:nth-child(1)').after('<td valign="top" width="173"><img height="20" src="/static/images/1x1.gif" width="20"></td>');
+    $('#password').closest('td').after(userId);
+    $('#password').closest('td').after('<td valign="top" width="90"><img height="20" src="/static/images/1x1.gif" width="20"></td>');
     $('#evolve_user_id_COPY').before('User ID: </br>');
 
     //Clone, enable, and align vst_guid field
     var vstId = $('#vst_guid').clone().attr('type', 'text').attr('disabled', 'true').attr('id', 'vst_guid_COPY').wrap('<td valign="top"></td>').parent().wrap('<tr></tr>').parent();
-    $('#accountForm > table > tbody > tr:nth-child(6) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(9)').after(vstId);
-    $('#accountForm > table > tbody > tr:nth-child(6) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(10)').before('<tr><td colspan="9" height="6"><img height="9" src="/static/images/1x1.gif"></td></tr>');
+    $('#password').closest('tr').after(vstId);
+    $('#password').closest('tr').after('<tr><td colspan="9" height="6"><img height="9" src="/static/images/1x1.gif"></td></tr>');
     $('#vst_guid_COPY').before('VST GUID: </br>');
 });
