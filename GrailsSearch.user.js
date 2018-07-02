21ce44900c44b28aeb7a25e1e07f797b05c504aa
U-SCIENCE\smit18 <smit18@ELSSTLW-7003333.science.regn.net>
2018-07-02 11:35:41 -0500

    Updated styling in GrailsSearch. Various small updates and fixes.

--- GrailsSearch.user.js
+++ GrailsSearch.user.js
@@ -1,13 +1,18 @@
 // ==UserScript==
 // @name         Grails Search
 // @namespace    http://lti-admin-ui.eols.io/
-// @version      0.4
+// @version      0.5
 // @description  Search Grails at your leisure.
 // @author       Paul Smith
 // @match        *lti-admin-ui.eols.io/*
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
-// @require      https://cdn.datatables.net/v/bs4/dt-1.10.16/datatables.min.js
-// @resource     customCSS https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css
+// @require      https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js
+// @require      https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js
+// @require      https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js
+// @require      https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js
+// @resource     customCSS1 https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css
+// @resource     customCSS2 https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css
+// @resource     customCSS3 https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // ==/UserScript==
@@ -15,8 +20,16 @@ var warn = 0;
 
 $(document).ready(function() {
     'use strict';
-    var newCSS = GM_getResourceText('customCSS');
-    GM_addStyle(newCSS);
+    var newCSS1 = GM_getResourceText('customCSS1');
+    var newCSS2 = GM_getResourceText('customCSS2');
+    var newCSS3 = GM_getResourceText('customCSS3');
+    GM_addStyle(newCSS1);
+    GM_addStyle(newCSS2);
+    GM_addStyle(newCSS3);
+    GM_addStyle('.pagination>li>a,.pagination>li>span{position:relative;float:left;padding:6px 12px;margin-left:-1px;line-height:1.42857143;color:#abbf78;text-decoration:none;background-color:#fff;border:1px solid #ddd}.pagination>.active>a,.pagination>.active>a:focus,.pagination>.active>a:hover,.pagination>.active>span,.pagination>.active>span:focus,.pagination>.active>span:hover{z-index:3;color:#fff;cursor:default;background-color:#abbf78;border-color:#333}');
+    $('body').css('margin', 'auto');
+    $('body').css('font-size', '16px');
+    $('table').css('border-spacing', '4px');
     var maxOffset;
     var url = window.location.href;
     var table;
